50f8e387c7045d09765bfc8477148648488e362a
Keks <keks@htmlacademy.ru>
2024-11-18 18:36:24 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/control-scale-picture.js
@@ -0,0 +1,37 @@
+// кнопка уменьшения масштаба
+const scaleControlSmallerElement = document.querySelector('.scale__control--smaller');
+// кнопка увеличения масштаба
+const scaleControlBiggerElement = document.querySelector('.scale__control--bigger');
+// поле отображения масштаба
+const scaleControlValueElement = document.querySelector('.scale__control--value');
+// изображение
+const imgUploadPreviewElement = document.querySelector('.img-upload__preview img');
+// шаг изменения масштаба
+const SCALE_STEP = 0.25;
+// начальное значение масштаба
+const START_SCALE = 1;
+// масштаб
+let scale = 1;
+
+// уменьшаем масштаб
+scaleControlSmallerElement.addEventListener('click', () => {
+  if (scale > 0.25 && scale <= 1) {
+    imgUploadPreviewElement.style.transform = `scale(${scale -= SCALE_STEP})`;
+    scaleControlValueElement.value = `${scale * 100}%`;
+  }
+});
+// увеличиваем масштаб
+scaleControlBiggerElement.addEventListener('click', () => {
+  if (scale >= 0.25 && scale < 1) {
+    imgUploadPreviewElement.style.transform = `scale(${scale += SCALE_STEP})`;
+    scaleControlValueElement.value = `${scale * 100}%`;
+  }
+});
+
+// функция сброса масштаба на начальное значение
+const resetScale = () => {
+  scale = START_SCALE;
+  imgUploadPreviewElement.style.transform = `scale(${scale})`;
+};
+
+export {resetScale};
---
+++ 11/js/main.js
@@ -0,0 +1,4 @@
+import './control-slider-effects.js';
+import './show-full-picture.js';
+import './control-scale-picture.js';
+import './upload-picture-form.js';
