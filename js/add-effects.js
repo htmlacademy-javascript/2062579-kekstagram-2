dd0a87773a5d963a993abffd01b4a668568f7599
savoyskiy <78842859+savoyskiy@users.noreply.github.com>
2025-12-03 21:48:57 +0300

    Дорабатывает функции установки эффектов после код-ревью

--- js/add-effects.js
+++ js/add-effects.js
@@ -1,45 +1,47 @@
 /* параметры эффектов */
-const NONE = { // без эффекта
-  MIN: 0,
-  MAX: 100,
-  STEP: 1,
-  EFFECT: 'none',
-  PARAM: ''
-};
-const CHROME = { // хром
-  MIN: 0,
-  MAX: 1,
-  STEP: 0.1,
-  EFFECT: 'grayscale',
-  PARAM: ''
-};
-const SEPIA = { // сепия
-  MIN: 0,
-  MAX: 1,
-  STEP: 0.1,
-  EFFECT: 'sepia',
-  PARAM: ''
-};
-const MARVIN = { // марвин
-  MIN: 0,
-  MAX: 100,
-  STEP: 1,
-  EFFECT: 'invert',
-  PARAM: '%'
-};
-const PHOBOS = { // фобос
-  MIN: 0,
-  MAX: 3,
-  STEP: 0.1,
-  EFFECT: 'blur',
-  PARAM: 'px'
-};
-const HEAT = { // хит
-  MIN: 1,
-  MAX: 3,
-  STEP: 0.1,
-  EFFECT: 'brightness',
-  PARAM: ''
+const EFFECTS = {
+  'none': { // без эффекта
+    MIN: 0,
+    MAX: 100,
+    STEP: 1,
+    EFFECT: 'none',
+    PARAM: ''
+  },
+  'chrome': { // хром
+    MIN: 0,
+    MAX: 1,
+    STEP: 0.1,
+    EFFECT: 'grayscale',
+    PARAM: ''
+  },
+  'sepia': { // сепия
+    MIN: 0,
+    MAX: 1,
+    STEP: 0.1,
+    EFFECT: 'sepia',
+    PARAM: ''
+  },
+  'marvin': { // марвин
+    MIN: 0,
+    MAX: 100,
+    STEP: 1,
+    EFFECT: 'invert',
+    PARAM: '%'
+  },
+  'phobos': { // фобос
+    MIN: 0,
+    MAX: 3,
+    STEP: 0.1,
+    EFFECT: 'blur',
+    PARAM: 'px'
+  },
+  'heat': { // хит
+    MIN: 1,
+    MAX: 3,
+    STEP: 0.1,
+    EFFECT: 'brightness',
+    PARAM: ''
+  }
 };
 const effectLevelSlider = document.querySelector('.effect-level__slider'); // слайдер
 const effectLevelContainer = document.querySelector('.img-upload__effect-level'); // контейнер слайдера
@@ -60,94 +62,56 @@ noUiSlider.create(effectLevelSlider, { // подключаем слайдер
   connect: 'lower'
 });
 
-const effectStyle = () => `${effectName}(${effectLevelValue.value}${effectParameter})`; // параметр для инлайн-стиля
+const effectStyle = () => { // параметр для инлайн-стиля
+  if (effectName === 'none') {
+    return `${effectName}`;
+  } else {
+    return `${effectName}(${effectLevelValue.value}${effectParameter})`;
+  }
+};
+
+const setEffect = (currentEffect) => { // функция установки значений эффектов
+  effectLevelSlider.noUiSlider.updateOptions({
+    range: {
+      min: EFFECTS[currentEffect].MIN,
+      max: EFFECTS[currentEffect].MAX
+    },
+    step: EFFECTS[currentEffect].STEP,
+  });
+  effectLevelSlider.noUiSlider.set(EFFECTS[currentEffect].MAX);
+  effectName = EFFECTS[currentEffect].EFFECT;
+  effectParameter = EFFECTS[currentEffect].PARAM;
+  uploadImagePreview.style.filter = effectStyle();
+  if (currentEffect === 'none') {
+    effectLevelContainer.classList.add('hidden'); // для отсутствия эффекта скрываем слайдер
+  } else {
+    effectLevelContainer.classList.remove('hidden');
+  }
+};
 
 const checkEffect = (evt) => { // функция выбора эффекта при клике по превьюшке
-  const checkedEffect = evt.target.id; // id выбранного эффекта
+  const checkedEffect = evt.target.value; // значение выбранного эффекта
 
   switch (checkedEffect) {
-    case 'effect-none':
-      effectLevelSlider.noUiSlider.updateOptions({
-        range: {
-          min: NONE.MIN,
-          max: NONE.MAX
-        },
-        step: NONE.STEP,
-      });
-      uploadImagePreview.style.filter = NONE.EFFECT;
-      effectLevelContainer.classList.add('hidden');
+    case 'none':
+      setEffect(checkedEffect);
       break;
-    case 'effect-chrome':
-      effectLevelSlider.noUiSlider.updateOptions({
-        range: {
-          min: CHROME.MIN,
-          max: CHROME.MAX
-        },
-        step: CHROME.STEP,
-      });
-      effectLevelSlider.noUiSlider.set(CHROME.MAX);
-      effectName = CHROME.EFFECT;
-      effectParameter = CHROME.PARAM;
-      uploadImagePreview.style.filter = effectStyle();
-      effectLevelContainer.classList.remove('hidden');
+    case 'chrome':
+      setEffect(checkedEffect);
       break;
-    case 'effect-sepia':
-      effectLevelSlider.noUiSlider.updateOptions({
-        range: {
-          min: SEPIA.MIN,
-          max: SEPIA.MAX
-        },
-        step: SEPIA.STEP,
-      });
-      effectLevelSlider.noUiSlider.set(SEPIA.MAX);
-      effectName = SEPIA.EFFECT;
-      effectParameter = SEPIA.PARAM;
-      uploadImagePreview.style.filter = effectStyle();
-      effectLevelContainer.classList.remove('hidden');
+    case 'sepia':
+      setEffect(checkedEffect);
       break;
-    case 'effect-marvin':
-      effectLevelSlider.noUiSlider.updateOptions({
-        range: {
-          min: MARVIN.MIN,
-          max: MARVIN.MAX
-        },
-        step: MARVIN.STEP,
-      });
-      effectLevelSlider.noUiSlider.set(MARVIN.MAX);
-      effectName = MARVIN.EFFECT;
-      effectParameter = MARVIN.PARAM;
-      uploadImagePreview.style.filter = effectStyle();
-      effectLevelContainer.classList.remove('hidden');
+    case 'marvin':
+      setEffect(checkedEffect);
       break;
-    case 'effect-phobos':
-      effectLevelSlider.noUiSlider.updateOptions({
-        range: {
-          min: PHOBOS.MIN,
-          max: PHOBOS.MAX
-        },
-        step: PHOBOS.STEP,
-      });
-      effectLevelSlider.noUiSlider.set(PHOBOS.MAX);
-      effectName = PHOBOS.EFFECT;
-      effectParameter = PHOBOS.PARAM;
-      uploadImagePreview.style.filter = effectStyle();
-      effectLevelContainer.classList.remove('hidden');
+    case 'phobos':
+      setEffect(checkedEffect);
       break;
-    case 'effect-heat':
-      effectLevelSlider.noUiSlider.updateOptions({
-        range: {
-          min: HEAT.MIN,
-          max: HEAT.MAX
-        },
-        step: HEAT.STEP,
-      });
-      effectLevelSlider.noUiSlider.set(HEAT.MAX);
-      effectName = HEAT.EFFECT;
-      effectParameter = HEAT.PARAM;
-      uploadImagePreview.style.filter = effectStyle();
-      effectLevelContainer.classList.remove('hidden');
+    case 'heat':
+      setEffect(checkedEffect);
       break;
   }
 };
 
-export { effectName, effectParameter, effectStyle, checkEffect };
+export { effectStyle, checkEffect };
