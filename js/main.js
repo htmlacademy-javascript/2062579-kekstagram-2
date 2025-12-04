20d8f7524bca1907f0f6efadd2cda8045a19ca2c
savoyskiy <78842859+savoyskiy@users.noreply.github.com>
2025-12-04 13:23:34 +0300

    Меняет порядок импорта в main

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
+import { photosArray } from './interaction-server.js'; // импорт данных с сервера
 import { createPictures } from './create-pictures.js'; // импорт функции, отрисовывающей изображения на странице
 import { onClickSmallPhoto } from './create-big-picture.js'; // импорт функции открытия/закрытия большого изображения
 import { onChangeImageInput } from './upload-photo.js'; // импорт функции загрузки изображения
 import { pristineValidateComment, pristineValidateHashtags } from './validation-form.js'; // импорт данных валидации полей ввода формы
 import { onChangeEffectStyle } from './add-effects.js'; // импорт данных для работы слайдера
-import { photosArray } from './interaction-server.js'; // импорт данных с сервера
 
 /* отрисовываем изображения по данным с сервера */
 createPictures(photosArray);
